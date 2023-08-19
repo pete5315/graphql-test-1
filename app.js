9d4c218392ce8f7afc1ec4c6255a5ee45ce65d51
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-08-18 19:05:27 -0500

    Server conneccted to database party emoji

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //import requirements for connecting to database
+const { json } = require("express");
 const { MongoClient } = require("mongodb");
-const uri = require("./atlas_uri")
+const uri = require("./atlas_uri");
 
 //set up the client to connect so we don't need to write things like db.connection.idontknowwhat
 const client = new MongoClient(uri);
 
 //database name inside of cluster
-const dbname = "boardgame-ranker"
+const dbname = "boardgame-ranker";
 
 //async function that just connects from the database and then disconnects, make sure your ip is whitelisted
 const connectToDatabase = async () => {
@@ -14,11 +15,96 @@ const connectToDatabase = async () => {
     await client.connect();
     console.log(`connected to the ${dbname} database`);
   } catch (err) {
-    console.log(`error connecting to the ${dbname} database: ${err}`)
+    console.log(`error connecting to the ${dbname} database: ${err}`);
   } finally {
     await client.close();
   }
-}
+};
+
+const readFromDatabase = async () => {
+  try {
+    await client.connect();
+    const database = client.db(`${dbname}`);
+    // const games = database.collection("games");
+    // const query = {};
+    // const options = {
+    //   // sort returned documents in ascending order by title (A->Z)
+    //   sort: { name: 1 },
+    //   // Include only the `name` and `url` fields in each returned document
+    //   projection: { _id: 0, name: 1, url: 1 },
+    // };
+    // const cursor = games.find(query, options);
+
+    // if ((await games.countDocuments(query)) === 0) {
+    //   console.log("No documents found!");
+    // }
+    // for await (const doc of cursor) {
+    //   console.dir(doc);
+    // }
+
+    const tm = await database.collection("games").find(
+      {},
+      {
+        // sort matched documents in descending order by url
+        sort: { url: -1 },
+        // Include only the `name` and `url` fields in the returned document
+        projection: { _id: 0, name: 1, url: 1 },
+      }
+    );
+    console.log(tm);
+    const gameList = tm.toArray();
+    console.log(gameList);
+  } catch (err) {
+    console.log(`error reading from the ${dbname} database: ${err}`);
+  } finally {
+    setTimeout(() => {
+      client.close();
+    }, 25000);
+    await client.close();
+  }
+
+  // try {
+  //   const database = client.db("sample_mflix");
+  //   const movies = database.collection("movies");
+  //   // query for movies that have a runtime less than 15 minutes
+  //   const query = { runtime: { $lt: 15 } };
+  //   const options = {
+  //     // sort returned documents in ascending order by title (A->Z)
+  //     sort: { title: 1 },
+  //     // Include only the `title` and `imdb` fields in each returned document
+  //     projection: { _id: 0, title: 1, imdb: 1 },
+  //   };
+  //   const cursor = movies.find(query, options);
+  //   // print a message if no documents were found
+  //   if ((await movies.countDocuments(query)) === 0) {
+  //     console.log("No documents found!");
+  //   }
+  //   for await (const doc of cursor) {
+  //     console.dir(doc);
+  //   }
+  // } finally {
+  //   await client.close();
+  // }
+};
+
+const writeToDatabase = async () => {
+  try {
+    await client.connect();
+    const database = client.db(`${dbname}`);
+
+    database.insertOne({
+      name: "Brass: Birmingham",
+      url: "aaa",
+    });
+  } catch (err) {
+    console.log(`error posting to the ${dbname} database: ${err}`);
+  } finally {
+    await client.close();
+  }
+};
 
 //invoke the connection to test it using 'node app.js' in the terminal
 connectToDatabase();
+
+//
+readFromDatabase();
---
+++ crud.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const uri = require("./atlas_uri");
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to the MongoDB Atlas database
+mongoose.connect(
+  `${uri}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Define your routes and endpoints here
+// Create a schema for your MongoDB documents
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+// Create a model based on the schema
+const User = mongoose.model("User", userSchema);
+
+// Define your routes and endpoints
+app.get("/users", async (req, res) => {
+  // Get all users
+  const users = await User.find();
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  console.log(req.body);
+  // Create a new user
+  const newUser = new User({
+    name: req.body.name,
+    age: req.body.age,
+  });
+  await newUser.save();
+  res.json(newUser);
+});
+
+app.put("/users/:id", async (req, res) => {
+  // Update a user by ID
+  const user = await User.findById(req.params.id);
+  user.name = req.body.name;
+  user.age = req.body.age;
+  await user.save();
+  res.json(user);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  // Delete a user by ID
+  await User.findByIdAndDelete(req.params.id);
+  res.sendStatus(204);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
